2b01db66e9953af3711f335fa3f9f5c5e7d2f6dd
Fernando Aponte <faponte1230@yahoo.com>
2023-07-12 22:36:57 -0400

    commit tree 355b12718b129bfc4cea271a221cf25ded386375

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Bob',2,'M','Brown', 'Calm', 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Syd',3,'F','Black', 'Aggressive', 1 );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Josh',2,'M','Blue', 'Calm', 1 );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Fern',4,'M','Brown', 'Playful', 1 );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Chris',3,'M','Red', 'Docile', 1 );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Erica',4,'F','Brown', 'PLayful', 1 );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Jen',3,'F','Blue', 'Aggressive', 1 );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES( NULL,1,'F','Brown', 'Playful', 1 );
